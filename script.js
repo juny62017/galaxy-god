2aecbc82b3c336ada18f9e40f8b1efe43df08e3c
juny62017 <juny62017@gmail.com>
2026-08-30 16:27:59 +0530

    build the basic typing page shell

---
+++ script.js
@@ -0,0 +1,6 @@
+const playField = document.querySelector("#playField");
+const wordLayer = document.querySelector("#wordLayer");
+const typeInput = document.querySelector("#typeInput");
+
+const scoreDisplay = document.querySelector("#scoreDisplay");
+const livesDisplay = document.querySelector("#livesDisplay");
